1a6e28fc4577dc55e1cd475334aff620be347bb9
LisboaCo <lisboa.co@v4company.com>
2026-04-17 16:32:28 -0300

    webhook-test2

--- assets/js/modules/form.js
+++ assets/js/modules/form.js
@@ -18,10 +18,9 @@
             };
 
             try {
-                await fetch('https://n8n.v4lisboatech.com.br/webhook/6e6301de-33f1-4116-8504-7b1cb4adf2b2', {
+                await fetch('https://n8n.v4lisboatech.com.br/webhook-test/6e6301de-33f1-4116-8504-7b1cb4adf2b2', {
                     method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(data)
+                    body: new URLSearchParams(data)
                 });
 
                 form.style.display = 'none';
